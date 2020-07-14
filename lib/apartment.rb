942d10b36ae320942374070433b06fa9b7c3f577
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-14 09:25:54 -0600

    Finish apartment class

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,9 +1,16 @@
 class Apartment
-  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
+
   def initialize(information)
     @number = information[:number]
     @monthly_rent = information[:monthly_rent]
     @bathrooms = information[:bathrooms]
     @bedrooms = information[:bedrooms]
-  end 
+    @renter = []
+  end
+
+  def add_renter(renter)
+    @renter << renter
+  end
+
 end
