8d9c36b15356e26844c519bc7b5928b0fb3de083
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-14 09:15:23 -0600

    Get renter tests to pass

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -21,25 +21,4 @@ class RenterTest < MiniTest::Test
 end
 
 
-# pry(main)> unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-# # => #<Apartment:0x00007fb3ecaae7c0...>
 #
-# pry(main)> unit1.number
-# # => "A1"
-#
-# pry(main)> unit1.monthly_rent
-# # => 1200
-#
-# pry(main)> unit1.bathrooms
-# # => 1
-#
-# pry(main)> unit1.bedrooms
-# # => 1
-#
-# pry(main)> unit1.renter
-# # => nil
-#
-# pry(main)> unit1.add_renter(renter1)
-#
-# pry(main)> unit1.renter
-# # => #<Renter:0x00007fb3ee106ce8...>
